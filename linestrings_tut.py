db5d6f5a7db5f403095f7126ff99a3417fcbc021
James Miller <james.rex.miller.4@gmail.com>
2025-03-26 21:13:53 -0600

    Add interpolated linestring

--- linestrings_tut.py
+++ linestrings_tut.py
@@ -3,8 +3,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 linestring = LineString([[2, 1], [1, 2], [1, 3], [0, 4]])
+middle = linestring.length / 2
+interpolated_linestring = linestring.interpolate(middle)
+
+
 plt.plot(linestring.xy[0], linestring.xy[1], color="orange")
 plt.scatter(linestring.xy[0], linestring.xy[1], s=200, color="orange", edgecolor="black")
+plt.scatter(interpolated_linestring.x, interpolated_linestring.y, s=200, edgecolor="black")
 plt.grid(ls="--", lw=1, alpha=0.6)
 plt.xticks(np.arange(-1, 4, 1))
 plt.yticks(np.arange(0, 6, 1))
