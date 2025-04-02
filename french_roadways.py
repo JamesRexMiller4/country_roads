85b223b168034758aee4ea4ad8eee8ee35a9026d
James Miller <james.rex.miller.4@gmail.com>
2025-04-01 18:37:50 -0600

    Roads of France with lines scaled by their distance to Parise

--- french_roadways.py
+++ french_roadways.py
@@ -1,5 +1,7 @@
 import geopandas as gpd
 import matplotlib.pyplot as plt
+from shapely import Point, LineString
+import numpy as np
 
 
 europe = gpd.read_parquet("./europe.parquet.gz")
@@ -13,10 +15,23 @@ europe = gpd.read_parquet("./europe.parquet.gz")
 
 df = gpd.read_file("./ne_10m_admin_0_countries/ne_10m_admin_0_countries.shp")
 gdf = df.loc[df['ADMIN'] == 'France']
+gdf2 = gpd.GeoDataFrame({'geometry': [Point(2.3522, 48.8466)]}, crs="EPSG:4326")
 
 roads = gpd.sjoin(europe, gdf, predicate='within')
 
-fig, ax = plt.subplots()
-roads.plot(ax=ax, lw=0.25)
+distances = roads.distance(gdf2.iloc[0].geometry)
+
+roads['distance2paris'] = distances
+print(roads)
+
+roads['distance2paris_lw'] = 1 / np.exp(roads['distance2paris'])
+
+leftSpan = np.amax(roads['distance2paris_lw']) - np.amin(roads['distance2paris_lw'])
+rightSpan = 0.9 - 0.05
+valueScaled = (roads['distance2paris_lw'] - np.amin(roads['distance2paris_lw'])) / leftSpan
+roads['distance2paris_lw'] = 0.05 + (valueScaled * rightSpan)
+
+fig, ax = plt.subplots(facecolor="#090909")
+roads.plot(ax=ax, column="GP_RTP", cmap="afmhot_r", lw=roads['distance2paris_lw'])
 ax.axis('off')
 plt.show()
